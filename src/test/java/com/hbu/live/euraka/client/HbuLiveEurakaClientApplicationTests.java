5294c34a023e39ccdfb965702772a85157d44d4e
e <1004337524@qq.com>
2018-06-17 12:24:36 +0800

    Setting the Environment

---
+++ src/test/java/com/hbu/live/euraka/client/HbuLiveEurakaClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hbu.live.euraka.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HbuLiveEurakaClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
